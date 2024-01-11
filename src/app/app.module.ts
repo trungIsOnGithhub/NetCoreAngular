fcb1b392fbf1a9c1fab2f299d8da13d35264cd9d
trungvuive <trung150502@gmail.com>
2024-01-11 08:48:24 +0700

    first component create and add manually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property/property-card.component';
+import { PropertyListComponent } from './property-list/property-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/property/property-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'property-card',
+    template: 'property-card.component.html',
+    styles: [`property-card.component.css`]
+})
+
+export class PropertyCardComponent {
+
+}
