6cc48cf4b959371bbb1c3ccdd8ad45b0bd671111
trungvuive <trung150502@gmail.com>
2024-01-13 07:57:31 +0700

    add base controller

---
+++ webapi/Controllers/BaseController.cs
@@ -0,0 +1,46 @@
+namespace webapi.Controllers
+{
+    public class PropertyController : BaseController
+    {
+        private readonly IUnitOfWork uow;
+
+        public PropertyController(IUnitOfWork uow)
+        {
+            this.uow = uow;
+        }
+
+        //property/list/1
+        [HttpGet("list/{sellRent}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetPropertyList(int sellRent)
+        {
+            var properties = await uow.PropertyRepository.GetPropertiesAsync(sellRent);
+            var propertyListDTO = mapper.Map<IEnumerable<PropertyListDto>>(properties);
+            return Ok(propertyListDTO);
+        }
+
+        //property/detail/1
+        [HttpGet("detail/{id}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetPropertyDetail(int id)
+        {
+            var property = await uow.PropertyRepository.GetPropertyDetailAsync(id);
+            var propertyDTO = mapper.Map<PropertyDetailDto>(property);
+            return Ok(propertyDTO);
+        }
+
+        //property/add
+        [HttpPost("add")]
+        [Authorize]
+        public async Task<IActionResult> AddProperty(PropertyDto propertyDto)
+        {
+            var property = mapper.Map<Property>(propertyDto);
+            var userId = GetUserId();
+            property.PostedBy = userId;
+            property.LastUpdatedBy = userId;
+            uow.PropertyRepository.AddProperty(property);
+            await uow.SaveAsync();
+            return StatusCode(201);
+        }
+    }
+}
