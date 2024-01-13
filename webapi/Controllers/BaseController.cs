bebf3130609defd3ac59bbaa9769f9a8f47e3098
trungvuive <trung150502@gmail.com>
2024-01-13 08:32:52 +0700

    update base controller

--- webapi/Controllers/BaseController.cs
+++ webapi/Controllers/BaseController.cs
@@ -1,46 +1,15 @@
-namespace webapi.Controllers
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
 {
-    public class PropertyController : BaseController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseController : ControllerBase
     {
-        private readonly IUnitOfWork uow;
-
-        public PropertyController(IUnitOfWork uow)
-        {
-            this.uow = uow;
-        }
-
-        //property/list/1
-        [HttpGet("list/{sellRent}")]
-        [AllowAnonymous]
-        public async Task<IActionResult> GetPropertyList(int sellRent)
-        {
-            var properties = await uow.PropertyRepository.GetPropertiesAsync(sellRent);
-            var propertyListDTO = mapper.Map<IEnumerable<PropertyListDto>>(properties);
-            return Ok(propertyListDTO);
-        }
-
-        //property/detail/1
-        [HttpGet("detail/{id}")]
-        [AllowAnonymous]
-        public async Task<IActionResult> GetPropertyDetail(int id)
-        {
-            var property = await uow.PropertyRepository.GetPropertyDetailAsync(id);
-            var propertyDTO = mapper.Map<PropertyDetailDto>(property);
-            return Ok(propertyDTO);
-        }
-
-        //property/add
-        [HttpPost("add")]
-        [Authorize]
-        public async Task<IActionResult> AddProperty(PropertyDto propertyDto)
-        {
-            var property = mapper.Map<Property>(propertyDto);
-            var userId = GetUserId();
-            property.PostedBy = userId;
-            property.LastUpdatedBy = userId;
-            uow.PropertyRepository.AddProperty(property);
-            await uow.SaveAsync();
-            return StatusCode(201);
+        // get claim to match with policy
+        protected int GetUserId() {
+            return int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
         }
     }
 }
